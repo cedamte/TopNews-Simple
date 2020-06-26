a50384bf90d8bda93dcb6d1121365b23ce328ad5
dami <dami4_u@msn.com>
2020-06-26 14:04:33 +0100

    Added Retrofit Service and NewsApi

---
+++ app/src/main/java/com/example/topnewssimple/api/TopHeadlinesServices.kt
@@ -0,0 +1,32 @@
+package com.example.topnewssimple.api
+
+import com.example.topnewssimple.BuildConfig
+import com.example.topnewssimple.data.topheadlines.TopHeadlines
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface TopHeadlinesServices {
+    @GET(BuildConfig.TOP_HEADLINES_ENDPOINT)
+    fun getTopHeadlines(@QueryMap(encoded = false) filters: Map<String, String>): Call<TopHeadlines>
+}
+
+val loggingInterceptor = HttpLoggingInterceptor().setLevel(
+    HttpLoggingInterceptor.Level.BASIC
+)
+
+val okHttpClient = OkHttpClient.Builder()
+    .addInterceptor(loggingInterceptor)
+    .build()
+
+
+val retrofit: Retrofit = Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
+    .client(okHttpClient)
+    .addConverterFactory(MoshiConverterFactory.create())
+    .build()
--- app/src/main/java/com/example/topnewssimple/ui/MainActivity.kt
+++ app/src/main/java/com/example/topnewssimple/ui/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.topnewssimple.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.topnewssimple.BuildConfig
 import com.example.topnewssimple.R
+import com.example.topnewssimple.api.TopHeadlinesServices
+import com.example.topnewssimple.api.retrofit
 import com.example.topnewssimple.data.topheadlines.Article.Companion.dummyArticle
+import com.example.topnewssimple.data.topheadlines.TopHeadlines
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
@@ -16,11 +24,30 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = TopHeadlinesAdapter()
 
+        retrofit.create(TopHeadlinesServices::class.java)
+            .getTopHeadlines(mapOf("country" to "gb", "apiKey" to BuildConfig.API_KEY))
+            .enqueue(object : Callback<TopHeadlines> {
+                override fun onFailure(call: Call<TopHeadlines>, t: Throwable) {
+                    Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG)
+                        .show()
+
+                }
+
+                override fun onResponse(
+                    call: Call<TopHeadlines>,
+                    response: Response<TopHeadlines>
+                ) {
+                    response.body()?.articles?.let { adapter.setData(it) }
+                }
+
+            })
+
+
         recyclerView = findViewById(R.id.rv_topHeadlines)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = adapter
 
-        adapter.setData(dummyArticle)
+
 
 
     }
